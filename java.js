a9812769a5032a15009436ca5f7664cc7708cdeb
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-11-04 23:44:32 -0500

    Finished fixing animation javascript

--- java.js
+++ java.js
@@ -10,15 +10,15 @@ closeMenu.addEventListener('click', ()=>{
 	navMenu.classList.remove('show')
 })
 
-document.addEventListener('mousemove', move)
+document.addEventListener('mousemove', move);
 
-function move(e) {
-	this.querySelectorAll('.move').forEach(Layer => {
+function move(e){
+	this.querySelectorAll('.move').forEach(layer =>{
 		const speed = layer.getAttribute('data-speed')
 
 		const x = (window.innerWidth - e.pageX*speed)/120
-		const y = (window.innerWidth - e.pageY*speed)/120
+		const y = (window.innerHeight - e.pageY*speed)/120
 
-		layer.style.transform = `translateX(${X}px) translateY(${x}px)`
+		layer.style.transform = `translateX(${x}px) translateY(${y}px)`
 	})
 }
