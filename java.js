acf54d71e9ddee64d6e69114095c5a0ae656b0fe
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-11-04 23:29:33 -0500

    Finished adding animation variables in HTML and Java

--- java.js
+++ java.js
@@ -13,5 +13,12 @@ closeMenu.addEventListener('click', ()=>{
 document.addEventListener('mousemove', move)
 
 function move(e) {
-	this.querySelectorAll('.move')
+	this.querySelectorAll('.move').forEach(Layer => {
+		const speed = layer.getAttribute('data-speed')
+
+		const x = (window.innerWidth - e.pageX*speed)/120
+		const y = (window.innerWidth - e.pageY*speed)/120
+
+		layer.style.transform = `translateX(${X}px) translateY(${x}px)`
+	})
 }
